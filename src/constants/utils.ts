83a19e31c7006f48b988eb001dd772c2f78af859
Silas de Paula <silasandrade94@gmail.com>
2021-05-25 10:09:56 -0300

    projeto react-nextsjs

---
+++ src/constants/utils.ts
@@ -0,0 +1,20 @@
+export const UTILS = {
+  bg: "https://miro.medium.com/max/875/1*htbUdWgFQ3a94PMEvBr_hQ.png",
+  title: "Quiz Nextjs",
+  description: "Teste os seus conhecimentos sobre o framework Nextjs",
+  questions: [],
+  external: [
+
+  ],
+  theme: {
+    colors: {
+      primary: "#000000",
+      secondary: "#bdb9b7",
+      mainBg: "#4f5b62",
+      contrastText: "#FFFFFF",
+      wrong: "#FF5722",
+      success: "#4CAF50"
+    },
+    borderRadius: "4px"
+  }
+}
